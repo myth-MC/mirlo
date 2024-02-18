eb46edc76d53580cc50910b824f9c6dd74eceb58
U8092 <uisl8092@gmail.com>
2024-02-18 20:48:29 +0100

    Make /watchdogreload executable from console

--- src/main/java/me/u8092/watchdog/commands/ReloadCommand.java
+++ src/main/java/me/u8092/watchdog/commands/ReloadCommand.java
@@ -9,8 +9,7 @@ import org.bukkit.entity.Player;
 public class ReloadCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(!(sender instanceof Player player)) return false;
-        if(!player.hasPermission("watchdog.reload")) return false;
+        if(!sender.hasPermission("watchdog.reload")) return false;
 
         Main.getInstance().reloadConfig();
         if(Main.getInstance().getConfig().getBoolean("debug")) Main.getInstance().getLogger().info("config.yml has been reloaded");
